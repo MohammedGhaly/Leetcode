254e5191b28c70fa20608af014bd1b32d28a7dbe
MohammedGhaly <medooghaly7@gmail.com>
2025-10-12 15:39:25 +0300

    ..66_plus_one

--- NeetCode 150/Math & Geometry/plus_one.py
+++ NeetCode 150/Math & Geometry/plus_one.py
@@ -1,13 +1,9 @@
 class Solution:
     def plusOne(self, digits: list[int]) -> list[int]:
-        for i in range(len(digits)-1, -1, -1):
-            new_digit = (digits[i] + 1) % 10
-            digits[i] = new_digit
-            
-            if new_digit > 0:
+        for i in range(len(digits) - 1, -1, -1):
+            digits[i] = (digits[i] + 1) % 10
+            if digits[i] > 0:
                 return digits
-            
         if digits[0] == 0:
             digits.insert(0, 1)
-        
         return digits
